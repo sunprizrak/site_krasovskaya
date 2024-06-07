cb482153b9c023d77a67690bf4b4294f7125a3e2
sunprizrak <sunprizrak@gmail.com>
2024-06-07 21:15:15 +0300

    ...

--- web/main/views.py
+++ web/main/views.py
@@ -4,5 +4,16 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = 'main/index.html'
     extra_context = {
-        'title': 'Красовская'
+        'title': 'Красовская',
+        'navbar_property': 'home-navbar',
+        'nav_link_color': 'home-color',
+    }
+
+
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
+    extra_context = {
+        'title': 'Обо Мне',
+        'navbar_property': 'about-navbar',
+        'nav_link_color': 'about-color',
     }
