de3ea5701d700af0d2837d3c663498eec0ad32cf
sunprizrak <sunprizrak@gmail.com>
2024-07-08 03:35:40 +0300

    ...

---
+++ web/news/static/news/js/like_news.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('.heart').click(function() {
+        var newsId = $(this).data('news-id');
+        var likeCount = $(this).find('span');
+
+        $.ajax({
+            url: '',
+            type: 'POST',
+            data: {
+                'news_id': newsId,
+                'csrfmiddlewaretoken': csrfToken,
+            },
+            success: function(data) {
+                if (data.success) {
+                    likeCount.text(data.likes);
+                } else {
+                    alert(data.error);
+                }
+            }
+        });
+    });
+});
