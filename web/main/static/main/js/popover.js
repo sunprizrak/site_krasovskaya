9c45906975b6a11446c1af5b67da457c6616d749
sunprizrak <sunprizrak@gmail.com>
2024-10-11 16:57:26 +0300

    ...

--- web/main/static/main/js/popover.js
+++ web/main/static/main/js/popover.js
@@ -1,43 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-
-    const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-        const popover = new bootstrap.Popover(popoverTriggerEl, {
-            trigger: 'manual',
-            html: true
-        });
-
-        let popoverTimeout;
-
-        // Показываем поповер при наведении на элемент
-        popoverTriggerEl.addEventListener('mouseenter', function () {
-            popover.show();
-            clearTimeout(popoverTimeout);
-
-            // Обработчик события для поповера
-            const popoverEl = document.querySelector('.popover');
-
-            if (popoverEl) {
-                popoverEl.addEventListener('mouseenter', function () {
-                    clearTimeout(popoverTimeout); // Останавливаем таймер для скрытия поповера
-                });
-
-                popoverEl.addEventListener('mouseleave', function () {
-                    popoverTimeout = setTimeout(function () {
-                        popover.hide(); // Скрываем поповер с задержкой
-                    }, 200);
-                });
-            }
-        });
-
-        // Скрываем поповер при уходе с элемента
-        popoverTriggerEl.addEventListener('mouseleave', function () {
-            popoverTimeout = setTimeout(function () {
-                const popoverEl = document.querySelector('.popover:hover');
-                if (!popoverEl) {
-                    popover.hide();
-                }
-            }, 200); // Даем небольшую задержку перед скрытием
-        });
-    });
-});
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl, {
+    trigger: 'focus'
+  })
+})
